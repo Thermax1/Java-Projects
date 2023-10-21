2b2ef624d378fd46464c101f100998aa94bb5755
Thermax1 <a.d.munoz23@gmail.com>
2023-10-21 19:39:31 -0400

    work

---
+++ Java-Bootcamp/SecondProgram/src/com/anthony/Main.java
@@ -0,0 +1,11 @@
+//package: those with the package below have access to the public class Main file and so on
+package com.anthony;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+        Scanner scan = new Scanner(System.in);
+    }
+}
