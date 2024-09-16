e10e807fdef03e0612412bb54fc8f2b12abb78cd
Thermax1 <a.d.munoz23@gmail.com>
2024-09-16 01:41:17 -0400

    cleaned up

--- Java-HeadFirst/Chapter 2 Objectville/TestDogDrive.java
+++
@@ -1,7 +0,0 @@
-public class TestDogDrive {
-    public static void main(String[] args) {
-        Dog d = new Dog();
-        d.size = 40;
-        d.bark();
-    }
-}
--- Java-HeadFirst/Chapter 2 Objectville/TestGuessDrive.java
+++
@@ -1,6 +0,0 @@
-public class TestGuessDrive {
-    public static void main(String[] args) {
-        GuessGame game1 = new GuessGame();
-        game1.StartGame();
-    }    
-}
