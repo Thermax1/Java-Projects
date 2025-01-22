0233b5ba76229001f18c0c244600fcbf9197dee0
Thermax1 <am15943n@pace.edu>
2025-01-22 12:57:15 -0500

    work

---
+++ Java-Dev-Bootcamp/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -0,0 +1,23 @@
+public class Biography {
+    public static void main(String[] args) {
+        
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
+
+
+        String name = "Anthony";
+        int age = 27;
+        String country = "USA";
+        String sport = "baseball";
+        int hours = 10;
+        String game = "chess";
+        String subject = "math";
+        char grade = 'A';
+     
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from "+country+". ");
+        System.out.println("My favorite sport is "+sport+". I play for "+hours+" hours a day.");
+        System.out.println("When I'm tired, I like to play "+ game+".");
+        System.out.println("In school, my favourite subject was "+subject+", I scored a "+grade+".");
+
+        // Compare your result to what's on Learn the Part.
+    }
+}
