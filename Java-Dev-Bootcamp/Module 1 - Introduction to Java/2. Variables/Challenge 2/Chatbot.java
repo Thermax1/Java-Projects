a677d72bd80d3a4d47d73aebcef10c6f66d64fe5
Thermax1 <a.d.munoz23@gmail.com>
2025-01-23 17:07:41 -0500

    work

--- Java-Dev-Bootcamp/Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Java-Dev-Bootcamp/Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -1,37 +1,41 @@
-//import Scanner
+import java.util.Scanner;
 
 public class Chatbot {
     public static void main(String[] args) {
         
         //See Learn the Part for the complete instructions. 
 
-        
+        Scanner scanner = new Scanner(System.in);
         //set up scanner. 
 
         System.out.println("Hello. What is your name?");
         //Pick up user's name and store it. 
-
+        String myName = scanner.nextLine();
                            
         //add new a line before asking next question. 
-        System.out.println("Hi <name>! I'm Javabot. Where are you from?");
+        System.out.println("\nHi " + myName + "! I'm Javabot. Where are you from?");
         //Pick up user's home and store it. 
-                        
+        
+        String myHome = scanner.nextLine();
 
         //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
+        System.out.println("\nI hear it's beautiful at " + myHome + "! I'm from a place called Oracle");
         System.out.println("How old are you?");
         //Pick up age and store it.
 
+        int myAge = scanner.nextInt();
+        scanner.nextLine();
 
         //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println("\nSo you're " + myAge +", cool! I'm 400 years old.");
+        System.out.println("This means I'm " + (double)(400 / myAge) + " times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
+        String favLang = scanner.nextLine();
 
 
         //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
+        System.out.println("\n" + favLang + ", that's great! Nice chatting with you, " + myName + ". I have to log off now. See ya!");
         
         //close scanner. 
 
