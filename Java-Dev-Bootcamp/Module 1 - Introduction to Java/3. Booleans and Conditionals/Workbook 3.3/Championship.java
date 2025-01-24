7eb147819719c9c5a36344ab5e54c0b82c634e2e
Thermax1 <am15943n@pace.edu>
2025-01-24 15:06:19 -0500

    work

--- Java-Dev-Bootcamp/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Java-Dev-Bootcamp/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -6,7 +6,18 @@ public class Championship {
         int gryffindor = 400;    //gryffindor points
         int ravenclaw = 200;    //ravenclaw points
 
-        // int margin = amount of points by which gryffindor scored over ravenclaw;
+        int margin = gryffindor - ravenclaw;
+
+        if(margin < -100){
+            System.out.println("In fourth place, Gryffindor!");
+        }
+        else if(margin >= -100 && margin < 1) {
+            System.out.println("In third place, Gryffindor!");
+        } else if(margin > 0 && margin < 300){
+            System.out.println("In second place, Gryffindor!");
+        } else {
+            System.out.println("Gryffindor takes the House Cup!");
+        }
         
     }
 }
