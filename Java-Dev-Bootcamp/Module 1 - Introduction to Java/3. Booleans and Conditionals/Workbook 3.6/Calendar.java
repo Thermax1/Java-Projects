84a19f2389e5121a930bcfe841159f1dd40cfcbd
Thermax1 <am15943n@pace.edu>
2025-01-27 15:30:17 -0500

    work

--- Java-Dev-Bootcamp/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
+++ Java-Dev-Bootcamp/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
@@ -3,13 +3,21 @@ public class Calendar {
         
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
-        String day = "Friday"; //Can be any day.
+        String day = "Tuesday"; //Can be any day.
         System.out.println("Hey, are you free on " + day + "? \n");
 
-
-        System.out.println("Hmm, let me check my calendar.");
+        System.out.println("Hmm, let me check my calendar.\n");
         
-        //Check 'calendar' here:        
+        switch (day) {
+            case "Monday": System.out.println("Sorry, I'm working late!"); break;
+            case "Tuesday": System.out.println("I have meetings all day!"); break;
+            case "Wednesday": System.out.println("I have a dentist appointment"); break;
+            case "Thursday": System.out.println("I have date night!"); break;
+            case "Friday": System.out.println("I'M FREE"); break;
+            case "Saturday": System.out.println("I'M FREE"); break;
+            case "Sunday": System.out.println("I'M FREE"); break;
+            default: System.out.println("Invalid day of the week. Misspelled?");
+        }     
         
 
     }
--- Java-Dev-Bootcamp/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
+++ Java-Dev-Bootcamp/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
@@ -10,27 +10,37 @@ public class Bank {
         System.out.println("Are you here to get a mortgage? (yes or no)");
         //Task 1 - Pick up the user's decision.
 
-
+        String mortgageDecision = scan.nextLine().toLowerCase();
         //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
+        if(mortgageDecision.equals("yes")){
+            System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
+            // Task 3 - Pick up each value 
+
+            int savings = scan.nextInt();
+            int creditDebt = scan.nextInt();
+            
             System.out.println("\nHow many years have you worked for?");
-               // Task 4 - Pick up number of years
+            // Task 4 - Pick up number of years
+            int yearsWorked = scan.nextInt();
 
             System.out.println("What is your name?");
-               // Task 5 - Pick up the user's name
+            // Task 5 - Pick up the user's name
+            scan.nextLine();
+            String userName = scan.nextLine();
                 
-               //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
+            //Task 6 - Approve the mortgage if they meet the requirements (see article)
+            //       - Otherwise, don't give them a mortgage.   
 
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
-
-
+            if(savings >= 10000 && creditDebt < 5000 && yearsWorked > 2){
+                System.out.println("Congratulations, " + userName + ", You have been approved!");
+            } else {
+                System.out.println("Sorry, you're not eligible for a mortgage!");
+            }
+        } else {
+            System.out.println("\nOK. Have a nice day!");
+        }
         scan.close();
     }
 }
