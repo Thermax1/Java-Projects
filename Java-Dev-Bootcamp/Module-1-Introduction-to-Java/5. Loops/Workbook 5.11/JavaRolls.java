9c76a5c881504b705111e0d09fb6eafb6ddde770
Thermax1 <am15943n@pace.edu>
2025-04-19 12:37:40 -0400

    workk

--- Java-Dev-Bootcamp/Module-1-Introduction-to-Java/5. Loops/Workbook 5.11/JavaRolls.java
+++ Java-Dev-Bootcamp/Module-1-Introduction-to-Java/5. Loops/Workbook 5.11/JavaRolls.java
@@ -9,7 +9,7 @@ public class JavaRolls {
 
         // See Learn the Part for detailed instructions.
 
-        System.out.print("Let's play Rolling Java. Type anything and press enter to start: ");
+        System.out.print("Let's play Rolling Java. Press enter to start: ");
 
         scan.nextLine();
 
@@ -17,13 +17,34 @@ public class JavaRolls {
         System.out.println("1. If you a roll a 6, the game stops.");
         System.out.println("2. If you a roll a 4, nothing happens.");
         System.out.println("3. Otherwise, you get 1 point.");
-        System.out.print("\nYou must collect at least 3 points to win. Enter anything and press enter to roll: ");
-
-        scan.nextLine();
+        System.out.print("\nYou must collect at least 3 points to win. Press enter to roll: ");
 
         while(my_main_loop){
-            
+            scan.nextLine();
+            diceRoll = rollDice();
+            System.out.println("You rolled a " + diceRoll);
+
+            if(diceRoll == 6){
+                System.out.println("End of game");
+                break;
+            }
+
+            else if(diceRoll == 4){
+                System.out.println("Zero points. Keep rolling.");
+            }
+            else{
+                score++;
+                System.out.println("One point keep rolling.");
+            }
         }
+        if(score >= 3){
+            System.out.println("\nWow, that's lucky! You win!");
+        }
+        else{
+            System.out.println("\nTough luck, you lose");
+        }
+
+        System.out.println("\nFinal score: " + score);
     }
 
     public static int rollDice() {
