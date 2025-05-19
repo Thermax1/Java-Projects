f7594727c97e4aac5c6240cdb25469413c757faa
Thermax1 <a.d.munoz23@gmail.com>
2025-05-19 15:39:55 -0400

    did it

--- Java-Dev-Bootcamp/Module-1-Introduction-to-Java/5. Loops/Pokerito/Pokerito.java
+++ Java-Dev-Bootcamp/Module-1-Introduction-to-Java/5. Loops/Pokerito/Pokerito.java
@@ -3,35 +3,33 @@ import java.util.Scanner;
 public class Pokerito {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        System.out.println(randomCard());
-        /*Task 2: Explain the rules
-
-        >>Let's play Pokerito. Type anything when you're ready.
-        |
-        >>It's like Poker, but a lot simpler.
-        >> (new line)
-        >> • There are two players, you and the computer.
-        >> • The dealer will give each player one card.
-        >> • Then, the dealer will draw five cards (the river)
-        >> • The player with the most river matches wins!
-        >> • If the matches are equal, everyone's a winner!
-        >> (new line)
-        >> • Ready? Type anything if you are.
-        |
-        */
-        
-
-        /*Task 3: Present the user with a card
-         println 'Here's your card:'
-         <show card>
-         <new line>
-         println 'Here's the computer's card:'
-         <show computer's card>
-        */
-
         int yourMatches = 0;
         int computerMatches =0;
+        String dealerCard = randomCard();
+        String userCard = randomCard();
+        String computerCard = randomCard();
+
+        /*Task 2: Explain the rules*/
+
+        System.out.println("Let's play Pokerito. Type anything when you're ready.");
+        System.out.println("It's like Poker, but a lot simpler");
+        System.out.println("1. There are two players, you and the computer");
+        System.out.println("2. The dealer will give each player one card");
+        System.out.println("3. Then, the dealer will draw five cards (the river).");
+        System.out.println("4. The player with the most river matches wins!");
+        System.out.println("5. If the matches are equal, everyone's a winner!");
+        System.out.println("\n Ready? Press enter if you are!");
+        scan.nextLine();
+        
+
+        /*Task 3: Present the user with a card*/
+
+        System.out.println("Here's your card");
+        System.out.println(userCard);
+        System.out.println("\n");
+        System.out.println("Here's the computer's card");
+        System.out.println(computerCard);
+
 
         /** Task 4 - Draw five cards
          * 
@@ -47,6 +45,23 @@ public class Pokerito {
          *      ...
          */
 
+        System.out.println("Now, the dealer will draw five cards. Press Enter to continue and make the dealer draw a card");
+        scan.nextLine();
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(dealerCard);
+            System.out.println("\n \n");
+            if(dealerCard.equals(computerCard)){
+                computerMatches++;
+            }
+            else if(dealerCard.equals(userCard)){
+                yourMatches++;
+            }
+            scan.nextLine();
+            dealerCard = randomCard();
+        }
+        
+
         /** Task 5 - Get the winner
          * 
          * • Count your number of matches.
@@ -59,7 +74,18 @@ public class Pokerito {
          * • If the matches are equal, print: everyone wins!.
          */
 
-         scan.close();
+        System.out.println("Your matches: " + yourMatches);
+        System.out.println("Computer matches: " + computerMatches);
+        if(yourMatches > computerMatches){
+            System.out.println("You win!");
+        }
+        else if(yourMatches < computerMatches){
+            System.out.println("You lose!");
+        }
+        else {
+            System.out.println("A tie!");
+        }
+        scan.close();
     }
 
     /** Task 1
@@ -72,7 +98,125 @@ public class Pokerito {
      *   2. Returns a card that matches the random number (get the String values from cards.text).   
      */
 
-     public static int randomCard(){
-        return (int)(Math.random() * 13) + 1;
+     public static String randomCard(){
+        int randomNum = (int)(Math.random() * 13) + 1;
+
+        switch (randomNum) {
+            case 1:
+            return
+                   "   _____\n"+
+                   "  |A _  |\n"+ 
+                   "  | ( ) |\n"+
+                   "  |(_'_)|\n"+
+                   "  |  |  |\n"+
+                   "  |____V|\n";
+          
+             case 2:
+             return
+                    "   _____\n"+              
+                    "  |2    |\n"+ 
+                    "  |  o  |\n"+
+                    "  |     |\n"+
+                    "  |  o  |\n"+
+                    "  |____Z|\n";  
+             case 3:
+             return
+                  "   _____\n" +
+                  "  |3    |\n"+
+                  "  | o o |\n"+
+                  "  |     |\n"+
+                  "  |  o  |\n"+
+                  "  |____E|\n";
+
+             case 4:
+             return 
+                   "   _____\n" +
+                   "  |4    |\n"+
+                   "  | o o |\n"+
+                   "  |     |\n"+
+                   "  | o o |\n"+
+                   "  |____h|\n";
+             case 5:
+             return
+                    "   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n";
+
+             case 6:
+             return
+                    "   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n";
+
+             case 7:
+              return
+                    "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n";
+             case 8:
+             return
+                    "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n";
+             
+             case 9:
+             return
+                    "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n";
+
+             case 10:
+             return
+                    "   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n";
+            
+             case 11:
+             return
+                    "   _____\n" +
+                    "  |J  ww|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o% |\n"+ 
+                    "  | | % |\n"+ 
+                    "  |__%%[|\n";
+            
+            case 12:
+            return
+                    "   _____\n" +
+                    "  |Q  ww|\n"+ 
+                    "  | o {(|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%O|\n";
+              
+              case 13:
+              return 
+                    "   _____\n" +
+                    "  |K  WW|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%>|\n";
+               default:
+               return "out of bounds somehow"; 
+        }
      }
 }
